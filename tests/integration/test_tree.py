dd824e5fbb504ed091d7cf119f03ef95aec9d88c
Becky Sweger <rsweger@umass.edu>
2024-10-22 15:58:32 -0400

    Add a tree property to Tree
    Also move the tree tests to the integration suite, since we're doing
    network connections.

---
+++ tests/integration/test_tree.py
@@ -0,0 +1,26 @@
+from urllib.parse import urlparse
+
+import pytest
+from cladetime import CladeTime, Tree
+from cladetime.exceptions import TreeNotAvailableError
+from freezegun import freeze_time
+
+
+def test__get_tree_url():
+    with freeze_time("2024-08-13 16:21:34"):
+        tree = Tree(CladeTime())
+        tree_url_parts = urlparse(tree.url)
+        assert "2024-07-17--12-57-03Z" in tree_url_parts.path
+        assert "tree.json" in tree_url_parts.path
+
+
+def test__get_tree_url_bad_date():
+    # we cannot get reference trees prior to 2024-08-01
+    with pytest.raises(TreeNotAvailableError):
+        Tree(CladeTime(tree_as_of="2024-07-13"))
+
+
+def test__get_reference_tree():
+    with freeze_time("2024-08-13 16:21:34"):
+        tree = Tree(CladeTime())
+        assert tree.tree.get("meta", "").get("title", "").lower() == "sars-cov-2 phylogeny"
