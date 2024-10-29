74e39a37959bc17850645be024d68e9199bca078
Becky Sweger <rsweger@umass.edu>
2024-10-29 14:25:21 -0400

    Add Docker check at startup

--- src/cladetime/__init__.py
+++ src/cladetime/__init__.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 import structlog
@@ -10,6 +11,9 @@ __all__ = ["CladeTime", "Tree"]
 
 # tells us package to consider DC a state
 os.environ["DC_STATEHOOD"] = "1"
+
+# whether or not Docker-dependent features are enabled
+DOCKER_FEATURES_ENABLED = False
 
 
 def setup_logging():
@@ -36,4 +40,23 @@ def setup_logging():
     )
 
 
+def check_docker_installed():
+    """Check if Docker is installed and running."""
+    try:
+        subprocess.run(["docker", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run(["docker", "info"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        docker_enabled = True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        msg = (
+            "WARNING: Docker is not installed on this machine, or it is not currently running.\n"
+            "Cladetime features that require Docker (for example, custom clade assignment) will "
+            "not be available."
+        )
+        docker_enabled = False
+        print(msg)
+
+    return docker_enabled
+
+
 setup_logging()
+DOCKER_FEATURES_ENABLED = check_docker_installed()
