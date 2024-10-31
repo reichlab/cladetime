84c9ed5a522297ec9b0fa7bbb3f2e0d327341979
Becky Sweger <rsweger@umass.edu>
2024-10-31 12:04:06 -0400

    Move test_sequence.py out of util folder
    Because sequence.py was moved out of util/ in a prior PR

--- tests/unit/test_sequence.py
+++ tests/unit/test_sequence.py
@@ -28,7 +28,7 @@ def test_file_path() -> Path:
     """
     Return path to the unit test files.
     """
-    test_file_path = Path(__file__).parents[2].joinpath("data")
+    test_file_path = Path(__file__).parents[1].joinpath("data")
     return test_file_path
 
 
