4c454b46410f008d944d263afe9c0b8ba456052a
Becky Sweger <rsweger@umass.edu>
2024-11-05 11:45:32 -0500

    Skip integration tests for assign_clades CLI
    The CLI portion of cladetime is not expected to work until
    we revamp it to use the NextClade-based methods for assigning
    clades. Furthermore, we need to add a service to the GitHub
    workflow that will allow Docker commands to run within the
    Docker-based CI process.

--- tests/integration/test_assign_clades.py
+++ tests/integration/test_assign_clades.py
@@ -1,11 +1,13 @@
 import datetime
 
 import click
+import pytest
 from cladetime.assign_clades import main
 from click.testing import CliRunner
 
 
 # test below runs the entire pipeline
+@pytest.mark.skip(reason="Skip until CLI is updated to use CladeTime.")
 def test_main(tmp_path):
     today = datetime.date.today()
     test_date = today - datetime.timedelta(days=2)
@@ -29,6 +31,7 @@ def test_main(tmp_path):
         assert result.exit_code == 0
 
 
+@pytest.mark.skip(reason="Skip until CLI is updated to use CladeTime.")
 def test_main_bad_date(tmp_path):
     today = datetime.date.today()
     test_date = today - datetime.timedelta(days=5)
