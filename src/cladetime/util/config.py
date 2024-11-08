ed4dc436bd80e6147688986d9328c8ac6cff7882
Becky Sweger <rsweger@umass.edu>
2024-11-08 14:59:30 -0500

    clean up unused config fields

--- src/cladetime/util/config.py
+++ src/cladetime/util/config.py
@@ -1,4 +1,4 @@
-from dataclasses import InitVar, asdict, dataclass, field
+from dataclasses import InitVar, asdict, dataclass
 from datetime import datetime, timezone
 from pathlib import Path
 from pprint import pprint
@@ -22,11 +22,6 @@ class Config:
     nextclade_data_url_version = "v3"
     nextclade_base_url: str = "https://nextstrain.org/nextclade/sars-cov-2"
     nextclade_input_tree_name: str = "tree.json"
-    reference_tree_file: Path | None = None
-    root_sequence_file: Path | None = None
-    assignment_no_metadata_file: Path | None = None
-    assignment_file: Path | None = None
-    assignment_file_columns: list[str] = field(default_factory=list)
 
     def __post_init__(
         self,
